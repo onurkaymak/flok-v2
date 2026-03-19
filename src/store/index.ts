c5c279a0c361b3124371fa721d3aefad59733bf7
Onur Kaymak <onurkaymak@outlook.com>
2026-03-19 13:26:26 -0700

    Add user, fleet, rental, and ui slices and wire up Redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import userReducer from "./slices/user-slice";
+import fleetReducer from "./slices/fleet-slice";
+import rentalReducer from "./slices/rental-slice";
+import uiReducer from "./slices/ui-slice";
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+    fleet: fleetReducer,
+    rental: rentalReducer,
+    ui: uiReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
