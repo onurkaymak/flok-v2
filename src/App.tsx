c36d9cd6170135b8064c11f5a9162e73c76498ba
Onur Kaymak <onurkaymak@outlook.com>
2026-03-20 10:49:51 -0700

    Set up routes and global notification UI in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router";
+import { useSelector } from "react-redux";
+import type { RootState } from "./store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+  const ui = useSelector((state: RootState) => state.ui.notification);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {ui && <>Notification</>}
+      <Routes>
+        {!isLoggedIn && <Route path="/" element={<>Home</>} />}
+        {!isLoggedIn && <Route path="/auth" element={<>Auth</>} />}
+        {isLoggedIn && <Route path="/" element={<Navigate to={"/profile"} />} />}
+        {isLoggedIn && <Route path="/profile" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/fleet" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/fleet/add" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/fleet/update" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/production" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/production/add" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/rental" element={<>Profile</>} />}
+        {isLoggedIn && <Route path="/profile/rental/add" element={<>Profile</>} />}
+        <Route path="*" element={<>404 Not Found</>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
