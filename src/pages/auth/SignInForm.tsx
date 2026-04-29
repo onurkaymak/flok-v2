1039512a79a3768724305e7ed7dd43e7b52dde15
Onur Kaymak <onurkaymak@outlook.com>
2026-04-29 11:43:59 -0700

    Fix input text padding in sign in form

--- src/pages/auth/SignInForm.tsx
+++ src/pages/auth/SignInForm.tsx
@@ -65,7 +65,7 @@ const SignInForm = ({ onCreateAccountButton }: Props) => {
                   autoComplete="off"
                   required
                   ref={emailInputRef}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3.5"
                 />
               </div>
             </div>
@@ -85,7 +85,7 @@ const SignInForm = ({ onCreateAccountButton }: Props) => {
                   autoComplete="off"
                   required
                   ref={passwordInputRef}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full px-3.5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
